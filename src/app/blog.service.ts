66049d4ad257ac00c90831f810668dae0b0a565c
Jiangtao682 <52443281+Jiangtao682@users.noreply.github.com>
2020-05-20 21:48:14 -0700

    doing delete part

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
-import { catchError} from 'rxjs/operators';
+import {catchError, max} from 'rxjs/operators';
+import {rejects} from 'assert';
+import {error} from '@angular/compiler/src/util';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +14,13 @@ export class BlogService {
   ) {
   }
 
-  private serverUrl: string = 'http://localhost:4200';
+  private serverUrl: string = '';
   private username: string = 'user2';
   public posts: Post[] = [];
 
-  fetch2(username: string): Promise<Post[]>{
-    return new Promise((resolve,reject) => {
-      fetch(this.serverUrl + '/api/' + this.username)
+  fetchPosts(username: string): Promise<Post[]>{
+    return new Promise<Post[]>((resolve,reject) => {
+      fetch(this.serverUrl + '/api/' + username)
         .then(response => response.json())
         .then(json => {
           console.log('sucessful fetch!');
@@ -29,24 +31,114 @@ export class BlogService {
           console.log('posts: ' , this.posts);
           resolve(this.posts)
         }).catch((error)=>{
-        console.error('Error: ', error);
+          console.error('Error:', error);
+          reject(error);
       })
     });
   }
+
+  getPosts(username:string, postid: number):Promise<Post>{
+    return new Promise<Post>((resolve,reject) => {
+      fetch(this.serverUrl + '/api/' + username + '/' + postid)
+        .then(response => response.json())
+        .then(json=> {
+          console.log('sucessful fetchPost!');
+          let post:Post = new Post(json);
+          resolve(post);
+        }).catch((error) => {
+          console.log('Error: ', error);
+          reject(error);
+      })
+    })
+  }
+
+  newPost(username: string, post: Post): Promise<void>{
+    const new_post =  new Post();
+    const maxId : number = Math.max(...this.posts.map((p: Post) => p.postid));
+    const new_postId = maxId + 1;
+    new_post.postid = new_postId;
+    this.posts.push(new_post);
+    return new Promise<void>((resolve, reject) => {
+      fetch(this.serverUrl + '/api/' + username + '/' + new_postId, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title: "", body: ""})
+      })
+        .then(response => {
+          if(response.status == 201){//sucess!
+            resolve();
+          }else{
+            this.posts.pop();
+            console.log('Fail to create a new post!')
+            reject(response.status);
+          }
+        }).catch(error => {
+          console.log('Error: ', error);
+          reject(error);
+      })
+    })
+  }
+
+  updataPost(username:string, post:Post): Promise<void>{
+    const target_index = this.posts.map((p: Post) => p.postid).indexOf(post.postid);
+    if (target_index !== -1){ // find the target post
+      let targetPost = this.posts[target_index];
+      targetPost.body = post.body;
+      targetPost.title = post.title;
+      targetPost.modified = Date.now();
+      return new Promise<void>((resolve,reject)=>{
+        fetch(this.serverUrl + '/api/' + username + '/' + post.postid, {
+          method: 'PUT',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(post)
+        })
+          .then(response => {
+            if(response.status == 200){// update sucess
+              resolve()
+            }else{
+              reject(404);
+            }
+          })
+      }).catch(error=>{
+        console.error('Error: ', error);
+      })
+    }else{
+      return null;
+    }
+  }
+
+  deletePost(username: string, postid: number): Promise<void>{
+    const target_index = this.posts.map((p: Post) => p.postid).indexOf(postid);
+    if (target_index !== -1) { // find the target post
+      return new Promise<void>((resolve, reject) => {
+        fetch(this.serverUrl + '/api/' + username + '/' + postid, {
+          method: 'DELETE',
+          credentials: 'include',
+        })
+      })
+    }
+  }
+
 
 }
 
 
 export class Post {
   postid: number;
-  created: Date;
-  modified: Date;
+  created: number;
+  modified: number;
   title: string;
   body: string;
   constructor(p?: Post) {
     this.postid = p && p.postid || 0;
-    this.created = p && new Date(p.created) || new Date();  // Date(value/Date)
-    this.modified = p && new Date(p.modified) || new Date();
+    this.created = p && p.created ||  Date.now();  // Date(value/Date)
+    this.modified = p && p.modified || Date.now();
     this.title = p && p.title || '';
     this.body = p && p.body || '';
   }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {BlogService, Post} from '../blog.service';
+import {catchError} from 'rxjs/operators';
 
 @Component({
   selector: 'app-test',
@@ -8,17 +9,49 @@ import {BlogService, Post} from '../blog.service';
 })
 export class TestComponent implements OnInit {
   posts: Post[];
+  post: Post;
+  updateTtestPost: Post = new Post({postid: 3, title: 'update',
+    body: 'update', created: 2, modified: 3});
 
   constructor(
     private blogService: BlogService
   ) { }
 
+// test for fetchPosts
   ngOnInit() {
-    const promise1 = this.blogService.fetch2('user2');
-    promise1.then(() => {
-        this.posts = this.blogService.posts;
-        console.log('function run');
-      });
+    this.fetchPosts();
   }
 
+  fetchPosts(): void {
+    const promise1 = this.blogService.fetchPosts('user2');
+    promise1.then(posts => {
+      this.posts = posts;
+    });
+  }
+
+// test for getPost
+  getPost(): void {
+    const promise1 = this.blogService.getPosts('user2', 4);
+    promise1.then(post => {
+      this.post = post;
+    });
+  }
+
+  newPost(){
+    const promise1 = this.blogService.newPost('user2', null);
+    promise1.then(() => {
+      console.log('Created a new post!');
+    }).catch(error => {
+      console.error(error);
+    });
+  }
+
+  updatePost(){
+    const promise1 = this.blogService.updataPost('user2', this.updateTtestPost);
+    promise1.then(() => {
+      console.log('Updated a post!');
+    }).catch(error => {
+      console.error(error);
+    });
+  }
 }
